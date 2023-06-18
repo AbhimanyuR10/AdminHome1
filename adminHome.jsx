f9eecefb10e7ce827f3ee35ee71787ed758613a4
AbhimanyuR10 <134275564+AbhimanyuR10@users.noreply.github.com>
2023-06-18 11:13:39 +0530

    Add files via upload

---
+++ adminHome.jsx
@@ -0,0 +1,85 @@
+import React, { Component, useEffect, useState } from "react";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
+export default function AdminHome({ userData }) {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        getAllUser;
+    },[]);
+
+    const getAllUser = () => {
+        fetch("http://localhost:8080/getAllUser", {
+          method: "GET",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data, "userData");
+            setData(data.data);
+          });
+      };
+
+
+      const logOut = () => {
+        window.localStorage.clear();
+        window.location.href = "./sign-in";
+      };
+
+      //deleting user
+  const deleteUser = (id, name) => {
+    if (window.confirm(`Are you sure you want to delete ${name}`)) {
+      fetch("http://localhost:8080/deleteUser", {
+        method: "POST",
+        crossDomain: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          userid: id,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          alert(data.data);
+          getAllUser();
+        });
+    } else {
+    }
+  };
+
+  return (
+    <div className="auth-wrapper" style={{ height: "auto" }}>
+      <div className="auth-inner" style={{ width: "auto" }}>
+        <h3>Welcom Admin</h3>
+        <table style={{ width: 500 }}>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>User Type</th>
+            <th>Delete</th>
+          </tr>
+          {data.map((i) => {
+            return (
+              <tr>
+                <td>{i.fname}</td>
+                <td>{i.email}</td>
+                <td>{i.userType}</td>
+                <td>
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    onClick={() => deleteUser(i._id, i.fname)}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+        <button onClick={logOut} className="btn btn-primary">
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+}
